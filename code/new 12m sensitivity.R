48461ae34dc002e6804d07d4493619592e1941a6
Kyle Roddick <kyle.roddick@gmail.com>
2021-07-04 11:05:36 -0300

    change short sample cor matrix to linear reg lines

--- code/new 12m sensitivity.R
+++ code/new 12m sensitivity.R
@@ -2280,6 +2280,81 @@ ssDat <- dat %>%
             deePrime = qnorm(qHitRate) - qnorm(qFARate)) %>%
   select(-qHitRate, -qFARate)
 
+chart.Correlation.linear <-
+  function (R, histogram = TRUE, method=c("pearson", "kendall", "spearman"), ...)
+  { # @author R Development Core Team
+    # @author modified by Peter Carl & Marek Lahoda
+    # Visualization of a Correlation Matrix. On top the (absolute) value of the correlation plus the result
+    # of the cor.test as stars. On botttom, the bivariate scatterplots, with a linear regression fit.
+    # On diagonal, the histograms with probability, density and normal density (gaussian) distribution.
+
+    x = checkData(R, method="matrix")
+
+    if(missing(method)) method=method[1] #only use one
+    cormeth <- method
+
+    # Published at http://addictedtor.free.fr/graphiques/sources/source_137.R
+    panel.cor <- function(x, y, digits=2, prefix="", use="pairwise.complete.obs", method=cormeth, cex.cor, ...)
+    {
+      usr <- par("usr"); on.exit(par(usr))
+      par(usr = c(0, 1, 0, 1))
+      r <- cor(x, y, use=use, method=method) # MG: remove abs here
+      txt <- format(c(r, 0.123456789), digits=digits)[1]
+      txt <- paste(prefix, txt, sep="")
+      if(missing(cex.cor)) cex <- 0.8/strwidth(txt)
+
+      test <- cor.test(as.numeric(x),as.numeric(y), method=method)
+      # borrowed from printCoefmat
+      Signif <- symnum(test$p.value, corr = FALSE, na = FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " "))
+      # MG: add abs here and also include a 30% buffer for small numbers
+      text(0.5, 0.5, txt, cex = cex * (abs(r) + .3) / 1.3)
+      text(.8, .8, Signif, cex=cex, col=2)
+    }
+
+    #remove method from dotargs
+    dotargs <- list(...)
+    dotargs$method <- NULL
+    rm(method)
+
+    hist.panel = function (x, ...=NULL ) {
+      par(new = TRUE)
+      hist(x,
+           col = "light gray",
+           probability = TRUE,
+           axes = FALSE,
+           main = "",
+           breaks = "FD")
+      lines(density(x, na.rm=TRUE),
+            col = "red",
+            lwd = 1)
+      # adding line representing density of normal distribution with parameters correponding to estimates of mean and standard deviation from the data
+      ax.x = seq(min(x), max(x), 0.1)                                                  # ax.x containts points corresponding to data range on x axis
+      density.est = dnorm(ax.x, mean = mean(x), sd = sd(x))   # density corresponding to points stored in vector ax.x
+      # lines(ax.x, density.est, col = "blue", lwd = 1, lty = 1)                                # adding line representing density into histogram
+      rug(x)
+    }
+
+    # Linear regression line fit over points
+    reg <- function(x, y, ...) {
+      points(x,y, ...)
+      abline(lm(y~x), col = "red")
+    }
+
+    # Draw the chart
+    if(histogram)
+      pairs(x, gap=0, lower.panel=reg, upper.panel=panel.cor, diag.panel=hist.panel)
+    else
+      pairs(x, gap=0, lower.panel=reg, upper.panel=panel.cor)
+  }
+
+png(here('figures', 'CorrelationMatrix.png'),
+    width = 8.5,
+    height = 8.5,
+    units = 'in',
+    res = 300)
+
 ssDat %>%
   # filter(StudyName != 'SEN1') %>%
   ungroup() %>%
@@ -2288,7 +2363,9 @@ ssDat %>%
          `False Alarm Rate` = FARate,
          `Response Bias` = respBias,
          `Sensitivity Index` = deePrime) %>%
-  chart.Correlation(method = 'spearman')
+  chart.Correlation.linear(method = 'spearman')
+
+dev.off()
 
 cor.test(ssDat$ShortSamples, ssDat$HitRate,
          method = 'spearman')
@@ -2437,6 +2514,6 @@ ssDat %>%
   theme(legend.position = c(.8, .8)
         , legend.title = element_blank())
 
-ggsave(here('plots', 'newNewPlots', 'shortSamples.png'),
+ggsave(here('figures', 'newNewPlots', 'shortSamples.png'),
        width = 8.5,
        height = 5.5)
